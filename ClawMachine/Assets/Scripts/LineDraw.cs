c662d08a41bc21945a75e05b69680a5eb2ed4863
vaibhavyxx <vd5443@rit.edu>
2025-10-12 22:03:04 -0400

    line renderer works

---
+++ ClawMachine/Assets/Scripts/LineDraw.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class LineDraw : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public Transform endPoint;
+    public Transform startPoint;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(lineRenderer == null)
+        {
+            lineRenderer = GetComponent<LineRenderer>();
+        }
+        lineRenderer.positionCount = 2;
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+        //lineRenderer.SetPosition(0, endPoint.position);
+        //lineRenderer.SetPosition(1, endPoint.position);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update the positions of the line renderer to match the start and end points
+        lineRenderer.SetPosition(0, startPoint.position);
+        lineRenderer.SetPosition(1, endPoint.position);
+    }
+}
--- ClawMachine/Assets/Scripts/Move.cs
+++ ClawMachine/Assets/Scripts/Move.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 
-enum Claw { Up, Down, Fixed};
+enum Claw { Up, Down, Idle, Grab};
 public class Move : MonoBehaviour
 {
     public float speed = 5f;
     [SerializeField] Input inputSystem;
+    [SerializeField] Transform claw;
     bool prevPressed = false;
     //bool goDown = false;
     public float goingDownPeriod = 5.0f;
     float currentTime = 0.0f;
-    Claw currentState = Claw.Fixed;
+    Claw currentState = Claw.Idle;
     bool clawPressed = false;
 
 
@@ -27,45 +28,49 @@ public class Move : MonoBehaviour
         float deltaTime = Time.deltaTime;
         float distance = speed * deltaTime;
 
+        //Goes down then up, but ideally should be down -> grab (atleast tries) -> up -> idle 
         if (currentState == Claw.Down)
         {
             currentTime += deltaTime;
-            Debug.Log("claw state: down" );
+            //Debug.Log("claw state: down" );
             dir.y = -1;
             if (currentTime > goingDownPeriod)
             {
 
+                ResetClawState(Claw.Grab);
+            }
+        }
+        else if (currentState == Claw.Grab)
+        {
+            currentTime += deltaTime;
+            dir.y = 0;
+            if (currentTime > goingDownPeriod)
+            {
                 ResetClawState(Claw.Up);
             }
         }
-        else if(currentState == Claw.Up) 
+        else if (currentState == Claw.Up)
         {
             currentTime += deltaTime;
             dir.y = 1;
-            if(currentTime > goingDownPeriod)
+            if (currentTime > goingDownPeriod)
             {
-                ResetClawState(Claw.Fixed);
+                ResetClawState(Claw.Idle);
             }
         }
+        else
+        {
+            dir.y = 0;
+        }
 
-        Debug.Log("time: " + currentTime);
-        Debug.Log("claw state: " + currentState);
-        Debug.Log(dir);
         this.transform.position += dir * distance;
         prevPressed = currentlyPressed;
     }
 
     void ResetClawState(Claw nextState)
     {
-        Debug.Log("claw transition");
+        //Debug.Log("claw transition");
         currentState = nextState;
-        currentTime = 0.0f;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Collided with " + collision.gameObject.name);
-        //goDown = false;
         currentTime = 0.0f;
     }
 }
