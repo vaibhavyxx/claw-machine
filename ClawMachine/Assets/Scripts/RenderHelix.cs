73cc89093b0fceb73876ecbcb085c71e828c0812
vaibhavyxx <vd5443@rit.edu>
2025-11-05 19:29:32 -0500

    fixed coil distorting itself eventually issue

--- ClawMachine/Assets/Scripts/RenderHelix.cs
+++ ClawMachine/Assets/Scripts/RenderHelix.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 public class Helix : MonoBehaviour
 {
     public LineRenderer _lineRenderer;
-    public GameObject sphere;
+
     public Vector3[] points;
     public float radius = 100.0f;
-    public float offset = 10.0f;
+    public float coilWidth = 10.0f;
     public float width = 0.1f;
+
     //Previous values
-    float prevWidth = 0.0f, prevRadius = 0.0f, prevOffset = 0.0f;
+    float prevWidth = 0.0f, prevRadius = 0.0f, prevOffset = 0.0f, prevTotal =0, prevSpeed = 0;
     public int total = 200;
-    //int count = 0;
+
+
     float theta = 0;
     float t = 0;
     float d = 0;
+
     public float speed = 1.0f;
     bool start = false;
 
@@ -29,10 +32,11 @@ public class Helix : MonoBehaviour
 
     void Update()
     {
-        if(prevWidth != width ||  prevRadius != radius || prevOffset!=offset)
-        {
+        //Resets the
+        if(prevWidth != width ||  prevRadius != radius || prevOffset!=coilWidth ||
+            prevTotal != total || prevSpeed != speed)
             start = true;
-        }
+        
         if (start)
         {
             t += Time.deltaTime;
@@ -41,19 +45,23 @@ public class Helix : MonoBehaviour
             d += (speed * t);
             for (int i = 0; i < total; i++)
             {
-                z += (offset * t);
+                z += (coilWidth * t);
                 Vector3 pt = new Vector3(
                 this.transform.position.x + Mathf.Cos(theta * i),
                 this.transform.position.y + Mathf.Sin(theta * i), z);
                 points[i] = (pt);
-                //Instantiate(sphere, pt, Quaternion.identity);
             }
             _lineRenderer.SetPositions(points);
             start = false;
         }
+        if (!start)
+            t = 0.0f;
+            
 
-        prevOffset = offset;
+        prevOffset = coilWidth;
         prevRadius = radius;
         prevWidth = width;
+        prevTotal = total;
+        prevSpeed = speed;
     }
 }
